31eac2381175ecae8c66554f259400ad925415c0
Jason Dunkelberger (dirkraft) <dirkraft@gmail.com>
2015-12-11 10:25:24 -0600

    Read repo meta

--- trekker-ui/src/main/webapp/factory/repos.github.factory.js
+++ trekker-ui/src/main/webapp/factory/repos.github.factory.js
@@ -6,11 +6,21 @@ function Repos(LocalCache, GitHub) {
   var Repos = this,
       cache = LocalCache.of('Repos');
 
+  Repos.get = cache.cachingPromise('get', get, true);
+  Repos.cacheGet = cache.cachingPromise('get', get);
   Repos.list = cache.cachingPromise('list', list, true);
   Repos.cacheList = cache.cachingPromise('list', list);
 
   return Repos;
 
+
+  /**
+   * @param org {String} organization name or organization/repo (will be detected)
+   * @param [repo] {String} repository name, assuming the first arg was not already the combined org/repo form
+   */
+  function get(org, repo) {
+    return GitHub.get('repos/' + (org.indexOf('/') > 0 ? org : org + '/' + repo));
+  }
 
   function list() {
     return GitHub.getAll('user/repos');
--- trekker-ui/src/main/webapp/viewctrl/top.home.controller.js
+++ trekker-ui/src/main/webapp/viewctrl/top.home.controller.js
@@ -1,27 +1,30 @@
 Trekker.controller('TopHomeCtrl', TopHomeCtrl);
 
-TopHomeCtrl.$inject = ['Tasks', 'Users', 'Settings'];
+TopHomeCtrl.$inject = ['Tasks', 'Users', 'Repos', 'Settings', '$q'];
 
-function TopHomeCtrl(Tasks, Users, Settings) {
+function TopHomeCtrl(Tasks, Users, Repos, Settings, $q) {
   var vm = this;
 
   vm.prefs = Settings.settings.viewPrefs.top_home;
   vm.user = {};
+  vm.repo = {};
   vm.fireTasks = [];
   vm.myTasks = [];
   vm.watchTasks = [];
 
-  vm.orgRepo = _.keys(_.pick(Settings.settings.repos, _.identity))[0]; // TODO placeholder
 
-  search(sprintf('repo:%s is:open label:fire', vm.orgRepo))
-      .then(bind(vm, 'fireTasks', 'items'));
+  $q.all([
+    Users.cacheCurrent(),
+    Repos.cacheGet(_.keys(_.pick(Settings.settings.repos, _.identity))[0]) // TODO placeholder
+  ]).then(function (results) {
+    vm.user = results[0];
+    vm.repo = results[1];
 
-  Users.cacheCurrent().then(function (user) {
-    vm.user = user;
-
-    search(sprintf('repo:%s is:open assignee:%s', vm.orgRepo, user.login))
+    search(sprintf('repo:%s is:open label:fire', vm.repo.full_name))
+        .then(bind(vm, 'fireTasks', 'items'));
+    search(sprintf('repo:%s is:open assignee:%s', vm.repo.full_name, vm.user.login))
         .then(bind(vm, 'myTasks', 'items'));
-    search(sprintf('repo:%s is:open involves:%s', vm.orgRepo, user.login))
+    search(sprintf('repo:%s is:open involves:%s', vm.repo.full_name, vm.user.login))
         .then(bind(vm, 'watchTasks', 'items'));
   });
 
