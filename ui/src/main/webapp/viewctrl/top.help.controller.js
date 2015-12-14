eadaf85bba2731f144f81b1e1f8af2bb7b9283ae
Jason Dunkelberger (dirkraft) <dirkraft@gmail.com>
2015-12-14 11:09:10 -0600

    Writing things down in the form of help.

--- ui/src/main/webapp/viewctrl/top.help.controller.js
+++ ui/src/main/webapp/viewctrl/top.help.controller.js
@@ -5,5 +5,37 @@ TopHelpCtrl.$inject = [];
 function TopHelpCtrl() {
   var vm = this;
 
-  
+  vm.treks = [
+    {
+      name: 'bug',
+      steps: ['found', 'fixed'],
+      description: ''
+    }, {
+      name: 'question',
+      steps: ['asked', 'answered'],
+      description: ''
+    }, {
+      name: 'feature',
+      steps: ['described', 'delivered'],
+      description: ''
+    }, {
+      name: 'task',
+      steps: ['needed', 'done'],
+      description: ''
+    }, {
+      name: 'idea',
+      steps: ['invented', 'vetted'],
+      description: ''
+    }
+  ];
+
+  vm.flags = [
+    {
+      name: 'debt',
+      description: ''
+    }, {
+      name: 'fire',
+      description: ''
+    }
+  ]
 }
